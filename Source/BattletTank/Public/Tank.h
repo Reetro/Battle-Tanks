07abb864b29e6ae6026e84f1df2532eeff7bd0bb
Reetro <thegamerboy7@gmail.com>
2019-11-25 19:00:25 -0700

    Added a launch speed value to Tanks for using in firing a projectial

--- Source/BattletTank/Public/Tank.h
+++ Source/BattletTank/Public/Tank.h
@@ -34,4 +34,7 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+  UPROPERTY(EditAnyWhere, Category = Firing)
+  float LaunchSpeed = 100000;
 };
