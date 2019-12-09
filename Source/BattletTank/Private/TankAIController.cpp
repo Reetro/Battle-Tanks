e00e90a64446a50de36519ca42f4017a81f0b1c9
Reetro <thegamerboy7@gmail.com>
2019-12-09 13:13:15 -0700

    Small AI Fixes

--- Source/BattletTank/Private/TankAIController.cpp
+++ Source/BattletTank/Private/TankAIController.cpp
@@ -41,7 +41,7 @@ void ATankAIController::SetPawn(APawn* InPawn)
   if (InPawn)
   {
     auto PossessedTank = Cast<ATank>(InPawn);
-    if (!ensure(PossessedTank)) { return; }
+    if (!PossessedTank) { return; }
 
     PossessedTank->OnDeath.AddUniqueDynamic(this, &ATankAIController::OnPossedTankDeath);
   }
